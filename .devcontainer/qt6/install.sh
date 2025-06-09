648985bb3c12cda67bb51fb8a67d75e2257b5788
Alex <alex.shaw.as@gmail.com>
2025-06-09 20:09:34 +0000

    ci: fix modules in devcontainer feature

--- .devcontainer/qt6/install.sh
+++ .devcontainer/qt6/install.sh
@@ -24,8 +24,9 @@ UPDATE_RC="${UPDATE_RC:-"true"}"
 # Support modules
 if [ ${#MODULES[@]} -gt 0 ]
 then
-    QT_MODULES=(`echo ${MODULES} | tr ',' ' '`)
+    QT_MODULES=$(echo ${MODULES[*]} | tr ',' ' ')
     QT_MODULES_FLAG="-m"
+    echo "(*) Modules: ${QT_MODULES}"
 fi
 
 # Runs apt-get update if needed.
@@ -123,7 +124,8 @@ then
 fi
 
 # Install
-echo "Installing Qt6 ${HOST} ${TARGET} ${VERSION} ${ARCH}..."
+echo "(*) Installing Qt6..."
+echo aqt install-qt ${HOST} ${TARGET} ${VERSION} ${ARCH} --outputdir "${INSTALL_DIR}" ${QT_MODULES_FLAG} ${QT_MODULES}
 aqt install-qt ${HOST} ${TARGET} ${VERSION} ${ARCH} --outputdir "${INSTALL_DIR}" ${QT_MODULES_FLAG} ${QT_MODULES}
 if (( $? > 0 ))
 then
